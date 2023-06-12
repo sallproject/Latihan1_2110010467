7b19522dd6094016fd15d26fa83b4757714e1f71
sallproject <mhmmd.faisal.mf.2123@gmail.com>
2023-06-12 21:29:59 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author ACER
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika faisal = new Matematika(6,7);
+        
+        System.out.println("Hasil Penjumlahan " +faisal.setPenjumlahan());
+        System.out.println("Hasil Pengurangan " +faisal.setPengurangan());
+        System.out.println("Hasil Perkalian " +faisal.setPerkalian());
+        System.out.println("Hasil Pembagian " +faisal.setPembagian());
+    }
 }
